a5a78bf273e3d7fe7ca3e6fe25009eff2ededc8a
JacobCunningham68 <43470160+JacobCunningham68@users.noreply.github.com>
2018-10-30 11:47:48 +0000

    Update NewUser.ps1

--- NewUser.ps1
+++ NewUser.ps1
@@ -2,7 +2,7 @@
 
 $FirstName = Read-Host 'What is the first name of the person'
 $Surname = Read-Host 'What is the surname'
-$Domain = '@fairtrade.org.uk'
+$Domain = '@XXXX'
 $EmailAddress = $FirstName + '.' + $Surname + $Domain
 $Username = $FirstName + '.' + $Surname
 $Fullname = $FirstName + $Surname
@@ -18,36 +18,35 @@ F - Volunteer'
 
 
 if ($Directorate -eq "A") {
-    $Path = "OU=CFO,OU=Ibex House Users,DC=fairtrade,DC=org,DC=uk"
+    $Path = "XXX"
 }
 elseif ($Directorate -eq "B") {
-    $Path = "OU=Impact,OU=Ibex House Users,DC=fairtrade,DC=org,DC=uk"
+    $Path = "XXX"
 }
 elseif ($Directorate -eq "C") {
-    $Path = "OU=Commercial,OU=Ibex House Users,DC=fairtrade,DC=org,DC=uk"
+    $Path = "XXX"
 }
 elseif ($Directorate -eq "D") {
-    $Path = "OU=Public Engagement,OU=Ibex House Users,DC=fairtrade,DC=org,DC=uk"
+    $Path = "XXX"
 }
 elseif ($Directorate -eq "E") {
-    $Path = "OU=Leadership Team,OU=Ibex House Users,DC=fairtrade,DC=org,DC=uk"
+    $Path = "XXX"
 }
 elseif ($Directorate -eq "F") {
-    $Path = "OU=Volunteers,OU=Ibex House Users - NonFF,DC=fairtrade,DC=org,DC=uk"
+    $Path = "XXX"
 }
 
 New-ADUser -Name $FullName -GivenName $FirstName -Surname $Surname -SamAccountName "$Username" -UserPrincipalName $EmailAddress -DisplayName $Displayname -Path $Path -AccountPassword(Read-Host -AsSecureString "Type Password for User") -Enabled $true
 
 Set-ADUser $Username -Add @{ProxyAddresses="smtp:$EmailAddress"} -ScriptPath 7windows.bat
 
-md \\olympus\data\FFUserFolders\$Username
-
+md \\Serverpath\path\path
 $Ar = New-Object  system.security.accesscontrol.filesystemaccessrule("$username","FullControl","Allow")
 $Acl.SetAccessRule($Ar)
-Set-Acl "\\olympus\data\FFUserFolders\$Username" $Acl
+Set-Acl "\\Serverpath\path\path" $Acl
 
 Write-Host = 'The account is now created. Add user to relevant security groups.'
 Write-Host = 'Creating Office 365 account and assigning license'
 
-Set-MsolUserLicense -UserPrincipalName $EmailAddress -AddLicenses "fairtradeuk:STANDARDWOFFPACK"
-Set-MsolUserLicense -UserPrincipalName $EmailAddress -AddLicenses "fairtradeuk:ADALLOM_O365"
+Set-MsolUserLicense -UserPrincipalName $EmailAddress -AddLicenses "domain:STANDARDWOFFPACK"
+Set-MsolUserLicense -UserPrincipalName $EmailAddress -AddLicenses "domain:ADALLOM_O365"
